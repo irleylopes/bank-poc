77ac7c4ddf6f3ab270cffd9c2fcd6d9a465686ec
Irley Lopes <irleylopes@gmail.com>
2020-01-23 21:50:24 -0300

    initial commit

---
+++ src/main/java/com/poc/bankpoc/BankPocApplication.java
@@ -0,0 +1,13 @@
+package com.poc.bankpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/bankpoc/BankPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.bankpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
